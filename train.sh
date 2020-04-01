2b5a03d6c8ac60b2969118966da84f2091742b48
evgenykashin <kashinevge@gmail.com>
2020-04-01 18:43:13 +0300

    Add usefull commands for training

---
+++ train.sh
@@ -0,0 +1,9 @@
+sudo docker run --rm -it --gpus all -v `pwd`:/app -p 8099:8099 tf15 /bin/bash
+cd app
+
+python dataset_tool.py create_from_images_raw datasets/afhq afhq --res_log2 9
+
+python run_training.py --num-gpus=2 --data-dir=~/datasets --config=config-e \
+  --dataset=afhq --mirror-augment=true
+
+python run_training.py --num-gpus=2 --data-dir=datasets --config=config-e   --dataset=afhq --mirror-augment=true --min-h=4 --min-w=4 --res-log2=7
